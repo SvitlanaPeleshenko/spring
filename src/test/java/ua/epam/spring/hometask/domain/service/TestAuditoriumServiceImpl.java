18f58a432ee241bd8ea7851c1c4c39f45813d3fa
SvitlanaPeleshenko <peleshenko.svitlana@gmail.com>
2019-05-08 19:04:44 +0300

    add auditorium service test

--- src/test/java/ua/epam/spring/hometask/domain/service/TestAuditoriumServiceImpl.java
+++ src/test/java/ua/epam/spring/hometask/domain/service/TestAuditoriumServiceImpl.java
@@ -4,58 +4,36 @@ import org.junit.Test;
 
 import javax.annotation.Resource;
 
+import ua.epam.spring.hometask.domain.Auditorium;
 import ua.epam.spring.hometask.domain.SetupTest;
-import ua.epam.spring.hometask.domain.User;
-import ua.epam.spring.hometask.service.UserService;
+import ua.epam.spring.hometask.service.AuditoriumService;
 
-import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 public class TestAuditoriumServiceImpl extends SetupTest {
 
+  private static final String AUDITORIUM_1_CODE = "white-hall";
+  private static final String AUDITORIUM_1_NAME = "White";
 
   @Resource
-  private UserService userService;
-
-  @Resource
-  private User newUser;
-
-  //test covers save(), getAll()
-  @Test
-  public void saveUserTest(){
-    userService.save(newUser);
-    User userFromService=userService.getById(newUser.getId());
-    assertTrue(userFromService.getFirstName().equals(newUser.getFirstName()));
-    assertTrue(userFromService.getLastName().equals(newUser.getLastName()));
-    assertTrue(userFromService.getBirthday().equals(newUser.getBirthday()));
-    assertTrue(userFromService.getEmail().equals(newUser.getEmail()));
-    assertTrue(userFromService.getBalance()==newUser.getBalance());
-  }
-
-
+  private AuditoriumService auditoriumService;
 
   @Test
-  public void getUserByIdTest(){
-    User userFromService=userService.getById(newUser.getId());
-    assertTrue(userFromService.getId().equals(newUser.getId()));
+  public void getAllAuditoriumsTest() {
+    assertFalse(auditoriumService.getAll().isEmpty());
   }
 
   @Test
-  public void getUserByEmailTest(){
-    User userFromService=userService.getUserByEmail(newUser.getEmail());
-    assertTrue(userFromService.getEmail().equals(newUser.getEmail()));
+  public void getByCodeTest() {
+    Auditorium auditorium = auditoriumService.getByCode(AUDITORIUM_1_CODE);
+    assertTrue(auditorium.getCode().equals(AUDITORIUM_1_CODE));
   }
 
   @Test
-  public void getAllUsersTest(){
-    assertTrue(userService.getAll().size()==1);
+  public void getByNameTest() {
+    Auditorium auditorium = auditoriumService.getByName(AUDITORIUM_1_NAME);
+    assertTrue(auditorium.getName().equals(AUDITORIUM_1_NAME));
   }
-
-  @Test
-  public void removeUserTest(){
-    userService.remove(newUser);
-    assertNull(userService.getById(newUser.getId()));
-  }
-
 
 }
---
+++ src/test/java/ua/epam/spring/hometask/domain/service/TestEventServiceImpl.java
@@ -0,0 +1,65 @@
+package ua.epam.spring.hometask.domain.service;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import javax.annotation.Resource;
+
+import ua.epam.spring.hometask.domain.SetupTest;
+import ua.epam.spring.hometask.domain.User;
+import ua.epam.spring.hometask.service.AuditoriumService;
+import ua.epam.spring.hometask.service.EventService;
+import ua.epam.spring.hometask.service.UserService;
+
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
+
+public class TestEventServiceImpl extends SetupTest {
+
+
+  @Resource
+  private EventService eventService;
+  @Resource
+  private AuditoriumService auditoriumService;
+
+  /*@Before
+  public void createUser() {
+    userService.save(newUser);
+  }
+
+
+  //test covers save(), getAll()
+  @Test
+  public void saveUserTest(){
+    User userFromService=userService.getById(newUser.getId());
+    assertTrue(userFromService.getFirstName().equals(newUser.getFirstName()));
+    assertTrue(userFromService.getLastName().equals(newUser.getLastName()));
+    assertTrue(userFromService.getBirthday().equals(newUser.getBirthday()));
+    assertTrue(userFromService.getEmail().equals(newUser.getEmail()));
+    assertTrue(userFromService.getBalance()==newUser.getBalance());
+  }
+
+  @Test
+  public void getUserByIdTest(){
+    User userFromService=userService.getById(newUser.getId());
+    assertTrue(userFromService.getId().equals(newUser.getId()));
+  }
+
+  @Test
+  public void getUserByEmailTest(){
+    User userFromService=userService.getUserByEmail(newUser.getEmail());
+    assertTrue(userFromService.getEmail().equals(newUser.getEmail()));
+  }
+
+  @Test
+  public void getAllUsersTest(){
+    assertTrue(userService.getAll().size()==1);
+  }
+
+  @Test
+  public void removeUserTest(){
+    userService.remove(newUser);
+    assertNull(userService.getById(newUser.getId()));
+  }*/
+
+}
